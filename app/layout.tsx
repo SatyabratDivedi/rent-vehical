13d7ec286e730dd9c5ecaf4917c72825b7dd05b9
Satyabrat Divedi <141771024+SatyabratDivedi@users.noreply.github.com>
2025-06-27 16:06:51 +0530

    og:image update

--- app/layout.tsx
+++ app/layout.tsx
@@ -341,6 +341,7 @@ export default function RootLayout({
 
         {/* Favicon and App Icons */}
         <link rel='icon' href='/favicon.ico' sizes='any' />
+        <meta property="og:image" content="/favicon.ico" />
         <link rel='icon' href='/icon.svg' type='image/svg+xml' />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
         <link rel='manifest' href='/manifest.json' />
